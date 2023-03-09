a55bad435cc4c5b0928155fe1b86c9071b278cdb
derekhaskell88 <122556697+derekhaskell88@users.noreply.github.com>
2023-03-08 21:26:33 -0700

    M09 participation final

--- M09_participation/HaskellDerek_text_scores-3.py
+++ M09_participation/HaskellDerek_text_scores-3.py
@@ -1,45 +1,81 @@
 #!/usr/bin/env python3
 
-def display_welcome():
-    print("The Test Scores program")
-    print("Enter 'x' to exit")
+def display_welcome():  # welcome message function
+    """
+    displays welcome message
+    """
+    print("The Test Scores program")  # welcome message
+    print("Enter 'x' to exit")  # instruction
     print("")
 
-def get_scores():
-    score_total = 0
-    counter = 0
-    while True:
-        score = input("Enter test score: ")
-        if score == "x":
-            return  score_total, counter
+
+def get_scores():  # get scores function
+    """
+    gets user input get scores
+
+    Returns:
+      scores after user types x  _type_: _description_
+    """
+    scores = []  # scores is a list
+    while True:  # start while loop
+        score = input("Enter test score: ")  # get user input
+        if score == "x":  # check is x to see if user is done inputting scores
+            return scores
         else:
-            score = int(score)
-            if score >= 0 and score <= 100:
-                score_total += score
-                counter += 1 
+            score = int(score)  # convert score to int
+            if score >= 0 and score <= 100:  # makes sure input is valid
+                scores.append(score)  # append user input to the scores list
             else:
                 print("Test score must be from 0 through 100. " +
-                      "Score discarded. Try again.")
+                      "Score discarded. Try again.")  # invalid message
 
-def process_scores(score_total, count):
+
+def process_scores(scores):
+    """
+    processes scores
+
+    Args:
+      takes user input and calculates average mean and median  scores (_type_): _description_
+    """
     # calculate average score
-    average = score_total / count
-                
+    scores.sort()
+    median = 0  # declaring variables
+    total = 0
+    for score in scores:  # start for loop using scores list as range
+        total += score  # declaring variable
+    num_scores = len(scores)  # declaring variable
+    average = total / num_scores  # calc average
+    low_score = min(scores)  # calc low score
+    high_score = max(scores)  # calc high score
+    median_index = num_scores // 2  # calc median index
+    if num_scores % 2 == 1:  # calc median
+        median = scores[median_index]  # calc median if odd
+    else:
+        middle_1 = scores[median_index]
+        middle_2 = scores[median_index - 1]
+        median = (middle_1 + middle_2) / 2  # calc median if even
+
     # format and display the result
     print()
-    print("Score total:       ", score_total)
-    print("Number of Scores:  ", count)
+    print("Score total:       ", total)
+    print("Number of Scores:  ", num_scores)
     print("Average Score:     ", average)
+    print("Low Score:     ", low_score)
+    print("High Score:     ", high_score)
+    print("Median Score:     ", median)
+
 
 def main():
+    """
+    main function 
+    """
     display_welcome()
-    score_total, count = get_scores()
-    process_scores(score_total, count)
+    scores = get_scores()
+    process_scores(scores)
     print("")
     print("Bye!")
 
+
 # if started as the main module, call the main function
-if __name__ == "__main__":
+if __name__ == "__main__":  # run main as main
     main()
-
-
--- M09_participation/myfirstapp.py
+++ M09_participation/myfirstapp.py
@@ -3,13 +3,18 @@ from tkinter import ttk, messagebox
 import math
 
 
-class polynomial_calculator(ttk.Frame):
+class function_calculator(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, padding="10 10 10 10")
         # self.pack(fill=tk.BOTH, expand=True)
 
         trinomial_calculator(parent).grid(row=0, column=0)
-        trinomial_calculator(parent).grid(row=0, column=1)
+        third_degree_calculator(parent).grid(row=0, column=1)
+        ttk.Button(parent, text="Exit", command=self.exit_app).grid(
+            row=1, column=1, sticky=tk.E, padx=5, pady=5)
+
+    def exit_app(self):
+        root.destroy()
 
 
 class trinomial_calculator(ttk.Frame):
@@ -25,7 +30,7 @@ class trinomial_calculator(ttk.Frame):
             row=3, column=0, sticky=tk.E)
         ttk.Label(self, text="Enter x value").grid(
             row=4, column=0, sticky=tk.E)
-        ttk.Button(self, text="Calc Int", command=self.calculate_int).grid(
+        ttk.Button(self, text="Calc Int", command=self.calculate_intercepts).grid(
             row=2, column=3)
         ttk.Button(self, text="Calc Vert", command=self.calculate_vertex).grid(
             row=3, column=3)
@@ -53,15 +58,15 @@ class trinomial_calculator(ttk.Frame):
         y = round((a*(x**2))+(b*x)+c, 2)
         self.results.config(text="Y = " + str(y))
 
-    def calculate_int(self):
+    def calculate_intercepts(self):
         a = float(self.leading_coefficient.get())
         b = float(self.second_coefficient.get())
         c = float(self.constant_term.get())
         step1 = math.sqrt(b**2 + -4*a*c)
-        step2 = (-1*b+step1)/2*a
-        step3 = (-1*b-step1)/2*a
-        x_int1 = round(step2, 2)
-        x_int2 = round(step3, 2)
+        step2 = (-1*b+step1)/(2*a)
+        step3 = (-1*b-step1)/(2*a)
+        x_int1 = round(step3, 2)
+        x_int2 = round(step2, 2)
         y_int = round((a*(0**2))+(b*0)+c, 2)
         self.results.config(text="X Int: " +
                             str(x_int1) + ", " + str(x_int2) + "\nY Int: " + str(y_int))
@@ -82,9 +87,78 @@ class trinomial_calculator(ttk.Frame):
         self.results.config(text="")
 
 
+class third_degree_calculator(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, padding="10 10 10 10")
+        ttk.Label(self, text="Third Degree Calculator").grid(
+            row=0, column=1, sticky=tk.E)
+        ttk.Label(self, text="Enter leading coefficient").grid(
+            row=1, column=0, sticky=tk.E)
+        ttk.Label(self, text="Enter second coefficient").grid(
+            row=2, column=0, sticky=tk.E)
+        ttk.Label(self, text="Enter third coefficient").grid(
+            row=3, column=0, sticky=tk.E)
+        ttk.Label(self, text="Enter constant term").grid(
+            row=4, column=0, sticky=tk.E)
+        ttk.Label(self, text="Enter x value").grid(
+            row=5, column=0, sticky=tk.E)
+        ttk.Button(self, text="Calc Int", command=self.calculate_intercepts).grid(
+            row=3, column=3)
+        ttk.Button(self, text="Calc Y",
+                   command=self.calculate_y).grid(row=4, column=3)
+        ttk.Button(self, text="Clear", command=self.clear).grid(
+            row=5, column=3)
+
+        self.leading_coefficient = ttk.Entry(self)
+        self.leading_coefficient.grid(row=1, column=1)
+        self.second_coefficient = ttk.Entry(self)
+        self.second_coefficient.grid(row=2, column=1)
+        self.third_coefficient = ttk.Entry(self)
+        self.third_coefficient.grid(row=3, column=1)
+        self.constant_term = ttk.Entry(self)
+        self.constant_term.grid(row=4, column=1)
+        self.x_value = ttk.Entry(self)
+        self.x_value.grid(row=5, column=1)
+        self.results = ttk.Label(self, text="")
+        self.results.grid(row=6, column=1)
+
+    def calculate_y(self):
+        a = float(self.leading_coefficient.get())
+        b = float(self.second_coefficient.get())
+        c = float(self.third_coefficient.get())
+        d = float(self.constant_term.get())
+        x = float(self.x_value.get())
+        y = round((a*(x**3))+(b*(x**2))+(c*x)+d, 2)
+        self.results.config(text="Y = " + str(y))
+
+    def calculate_intercepts(self):
+        a = float(self.leading_coefficient.get())
+        b = float(self.second_coefficient.get())
+        c = float(self.constant_term.get())
+        d = float(self.third_coefficient.get())
+        # y_int = round(a*(0**3))+(b*(0**2)+(c*0)+d, 2)
+        x = 0
+        while x < 3:
+            x += .1
+            y = (a*(x**3))+(b*(x**2))+(c*x)+d
+            if y == 0:
+                return x
+            break
+
+        self.results.config(text="X Int: " + str(x))
+
+    def clear(self):
+        self.leading_coefficient.delete(0, tk.END)
+        self.second_coefficient.delete(0, tk.END)
+        self.third_coefficient.delete(0, tk.END)
+        self.constant_term.delete(0, tk.END)
+        self.x_value.delete(0, tk.END)
+        self.results.config(text="")
+
+
 if __name__ == "__main__":
     root = tk.Tk()
-    polynomial_calculator(root)
+    function_calculator(root)
     # root.geometry("500x500")
     root.title("Derek Haskell's Polynomial Calculator")
     root.mainloop()
