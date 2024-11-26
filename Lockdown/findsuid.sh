38a8cf61f3ff117d41eed2002d39a9d6ad8025e9
derekhaskell88 <122556697+derekhaskell88@users.noreply.github.com>
2024-11-25 22:12:39 -0700

    Delete Lockdown directory

--- Lockdown/findsuid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# run as standard user
-timestamp=$(date +"%H-%M")
-output_file="suid-rep-$timestamp.txt"
-echo -e "\e[32mFinding SUID binaries"
-find / -type f -perm /4000 -exec ls -l {} \; > "$output_file" 2>/dev/null
-echo -e "Complete!"
-echo -e "SUID Report generated in $output_file \e[0m"
